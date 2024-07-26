5fb60941ed8cb629c112a83e8da2675e9f5ab981
Juwel Rana <developerjuwel792@gmail.com>
2024-07-26 21:23:49 +0600

    add:database Context

--- LMS.Infrastructure/DatabaseContext/ApplicationDbContext.cs
+++ LMS.Infrastructure/DatabaseContext/ApplicationDbContext.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LMS.Domain.Model.IdentityModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS.Infrastructure.DatabaseContext;
 
-public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-     modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
-     //modelBuilder.Model.GetEntityTypes().SelectMany(x=>x.GetForeignKeys()).ToList().ForEach(x=>base.OnModelCreating(modelBuilder));
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        modelBuilder.Model.GetEntityTypes().SelectMany(x => x.GetForeignKeys()).ToList().ForEach(x => base.OnModelCreating(modelBuilder));
     }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
